81323cc8e9c95b84580745d295d21784f63435c1
LiZeC <tomgreem123@gmail.com>
2018-03-20 23:37:55 +0800

    添加编译预处理器测试用例

---
+++ test/testMacro.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../stdlib/lscio.h"
+
+#define MAX_SIZE 10
+#define acd 12
+
+int main()
+{
+    int a[MAX_SIZE];
+
+    for(int i=0;i<10;i++){
+        char* str = "MAX_SIZE";
+    }
+    int b = 0xacd;
+    int maybe = acd;
+}
