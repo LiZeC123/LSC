7421e2f0089728a150a58c7ad9dd4888f459de72
LiZeC <tomgreem123@gmail.com>
2018-04-29 09:52:39 +0800

    添加编译参数-h和-o

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 #include <string>
 #include <unistd.h>
 #define SIZE 256
-//#define _LSC_DEBUG_
+
+//#define __LSC_DEBUG__
+#define __LSC_VERSION__ "1.0.0"
+
 using namespace std;
 
 // 手动取出以下函数的定义,从而避免vscode的语法检查无法识别头文件函数定义的问题
@@ -20,6 +23,7 @@ class CompileFile
 public:
 	static string currPath;
 	CompileFile(string fullname);
+	CompileFile(const char* fullname);
 	const string& getCoreName() const;
 	const string& getType() const;
 	const string& getFullName() const;
@@ -46,10 +50,13 @@ CompileFile::CompileFile(string name)
 		type = fullName.substr(pos);	
 	}
 	else{
-		coreName = "";
+		coreName = name;
 		type = "";
 	}
 }
+
+CompileFile::CompileFile(const char* name) : CompileFile(string(name))
+{ }
 
 string CompileFile::getCurrWorkPath()
 {
@@ -73,6 +80,14 @@ const string& CompileFile::getFullName() const
 	return fullName;
 }
 
+
+class Args
+{
+public:
+	bool isHelped = false;
+	bool isSetOutput = false;
+	string outputName;
+};
 
 
 // 获得本程序对应的可执行文件实际的位置
@@ -123,9 +138,72 @@ void execCmd(const string& path, const string& cmd, const string& file)
 	sprintf(cmdBuf, "%s%s %s", path.c_str(),cmd.c_str(),file.c_str());
 	system(cmdBuf);
 
-#ifdef _LSC_DEBUG_
+#ifdef __LSC_DEBUG__
 	printf("Run Shell Command -> %s\n",cmdBuf);
 #endif
+}
+
+void printHelpInfo()
+{
+	printf("Lsc %s (on %s,%s)\n", __LSC_VERSION__,__DATE__, __TIME__);
+	printf("[GCC %d.%d.%d] on Ubuntu\n",__GNUC__,__GNUC_MINOR__,__GNUC_PATCHLEVEL__);
+	printf("Lsc iS Compile!\n\n");
+	printf("指令格式 lsc [options] file ...\n");
+	printf("-h 显示此信息\n");
+	printf("-o 指定输出文件名\n");
+	//printf("");
+	//printf("");
+}
+
+void analyseOptions(Args& args, int argc, char* argv[])
+{
+	for(int i=0;i<argc;i++){
+		if(argv[i][0] == '-'){
+			string option(argv[i]);
+
+			if(option.size() == 1){
+				printf("选项不能为空\n");
+			}
+			else if(option[1] == 'o'){
+				args.isSetOutput = true;
+				if((i+1) < argc){
+					args.outputName = string(argv[i+1]);
+				}
+				else{
+					printf("选项: -o 后必须指定文件名\n");
+				}
+			}
+			else if(option[1] == 'h') {
+				args.isHelped = true;
+				printHelpInfo();
+			}
+			else{
+				printf("选项: %s 无效\n",argv[i]);
+			}
+		}
+	}
+}
+
+void analyseFile(vector<CompileFile>& compilefiles, int argc, char* argv[])
+{
+    for(int i=1;i<argc;i++){
+		if(argv[i][0] != '-'){
+			if(argv[i-1] == string("-o")){
+				// 此文件是通过-o参数指定的输出文件, 因此忽略
+				continue;
+			}
+
+			CompileFile cfile = CompileFile(argv[i]);
+			if(!checkType(cfile.getType())){
+				printf("%s%s不是正确格式的文件, 已自动跳过此文件"
+					, cfile.getCoreName().c_str()
+					, cfile.getType().c_str());
+			}
+			else{
+				compilefiles.push_back(cfile);
+			}
+		}
+    }
 }
 
 
@@ -135,31 +213,20 @@ int main(int argc,char* argv[])
         return 0;
     }
 
-
-	string exePath = getFilePath();
-
-
+	Args args;
 	vector<CompileFile> compilefiles;
 
-    for(int i=1;i<argc;i++){
-		string file = string(argv[i]);
+	analyseOptions(args,argc,argv);
+	analyseFile(compilefiles,argc,argv);
 
-		if(file[0] == '-'){
-			//.. 指令
-		}
-		else{
-			CompileFile cfile = CompileFile(file);
-			if(!checkType(cfile.getType())){
-				printf("%s.%s不是正确格式的文件, 已自动跳过此文件"
-					, cfile.getCoreName().c_str()
-					, cfile.getType().c_str());
-			}
-			else{
-				compilefiles.push_back(cfile);
-			}
-		}
-    }
+
+	if(args.isHelped && compilefiles.size()==0){
+		return 0;
+	}
+
+
    
+   string exePath = getFilePath();
 	// 编译, 汇编
 	for(const auto& file: compilefiles){
 		if(file.getType() == ".c"){
@@ -183,5 +250,9 @@ int main(int argc,char* argv[])
 	}
 	execCmd(exePath,"/lit/lscl", allfiles);
 	execCmd("","chmod +x z.out","");
+
+	if(args.isSetOutput){
+		execCmd("","mv z.out "+args.outputName,"");
+	}
 }
 
