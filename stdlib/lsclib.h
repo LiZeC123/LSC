ad37f4c2bd1eab74a2064b4422cba1d12a05436b
LiZeC <tomgreem123@gmail.com>
2018-08-13 20:42:32 +0800

    添加内存分配有关的函数

--- stdlib/lsclib.h
+++ stdlib/lsclib.h
@@ -7,6 +7,22 @@
 void exit(int code);
 
 /**
+ * [函数名] lscmalloc
+ * [效果] 分配制定大小的内存空间
+ * [参数] size : 指定的内存空间的大小
+ * [返回值] 分配的空间的首地址
+ */
+char* lscmalloc(int size);
+
+/**
+ * [函数名] lscfree
+ * [效果] 释放分配的空间
+ * [参数] size : 有lscmalloc分配的空间首地址
+ * [返回值] 无
+ */
+void lscfree(char* addr);
+
+/**
  * [函数名] lscstr2int
  * [效果] 将字符串转化为数字
  * [参数] str   : 存放待转换的字符串
--- stdlib/lsclib.s
+++ stdlib/lsclib.s
@@ -10,6 +10,45 @@ exit:
 	mov esp,ebp
 	pop ebp
 	ret
+global lscmalloc
+lscmalloc:
+    push ebp
+	mov ebp,esp
+	
+    mov eax, 192        ; sys_mmap_pgoff  参考 https://blog.csdn.net/u014089131/article/details/54574964
+    mov ebx, 0    	    ; addr = NULL
+    mov ecx, [ebp+8]    ; len
+	add ecx, 8			; 申请额外的空间
+    mov edx, 0x7        ; prot = PROT_READ|PROT_WRITE|PROT_EXEC
+    mov esi, 0x22       ; flags = MAP_PRIVATE|MAP_ANONYMOUS
+    mov edi, 0xffffffff ; fd = -1
+    push ebp            ; 接下来要使用ebp,因此先保存到栈
+    mov ebp, 0			; offset = 0
+    int 128             ; 系统调用申请内存,返回值保存到eax
+	pop ebp            	; 
+
+	mov ebx, 0x4d43534c	; magin number LSCM
+	mov [eax],ebx
+	mov [eax+4],ecx		; 分配的内存大小
+	add eax,8
+   
+	mov esp,ebp
+	pop ebp
+	ret
+global lscfree
+lscfree:
+    push ebp
+	mov ebp,esp
+
+    mov eax, 91         ; munmap
+    mov ebx, [ebp+8]    ; addr
+    sub ebx,8           ; 恢复首地址位置
+    mov ecx, [ebx+4]    ; 取出保存的大小
+    int 128             ; 系统调用释放内存
+    
+    mov esp,ebp
+	pop ebp
+	ret
 global lscint2str
 lscint2str:
 	push ebp
