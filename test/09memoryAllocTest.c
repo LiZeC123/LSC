ee30063a7c695308de750c2006a6f28f7db1d338
liuzheng <tomgreem123@gmail.com>
2020-06-10 09:26:26 +0800

    添加批量分配内存的测试

--- test/09memoryAllocTest.c
+++ test/09memoryAllocTest.c
@@ -2,26 +2,58 @@
 
 #define SIZE 26
 
-int main()
+void TestBase()
 {
-
     char *arr = lscmalloc(SIZE + 2);
     char *base = "abcdefghijklmnopqrstuvwxyz";
 
-    int i = 0;
-    while (i < SIZE)
+    for (int i = 0; i < SIZE; i++)
     {
         arr[i] = 'a' + i;
-        i++;
     }
     arr[SIZE] = '\0';
 
-    checkStringEquals(arr,base,__LINE__);
+    checkStringEquals(arr, base, __LINE__);
 
     lscfree(arr);
+}
 
+void TestMany()
+{
+#define T_SIZE 1024
+    char *arr[T_SIZE];
 
-    // 批量分配测试, 例如分配 1024 * 1KB 空间
+    for (int i = 0; i < T_SIZE; i++)
+    {
+        // 测试大批量分配
+        arr[i] = lscmalloc(T_SIZE);
+    }
 
+    for (int i = 0; i < T_SIZE; i++)
+    {
+        // 测试对分配的内存写入数据
+        char *m = arr[i];
+        m[i] = i % 256;
+    }
+
+    for (int i = 0; i < T_SIZE; i++)
+    {
+        // 测试从分配的内存读取数据
+        char *m = arr[i];
+        char v = i % 256;
+        checkCharEquals(m[i], v, __LINE__);
+    }
+
+    for (int i = 0; i < T_SIZE; i++)
+    {
+        // 测试释放
+        lscfree(arr[i]);
+    }
+}
+
+int main()
+{
+    TestBase();
+    TestMany();
     return 0;
 }
