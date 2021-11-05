cc1b1ef4dc73a2e7ebdf97fc13547a236969c790
LiZeC <tomgreem123@gmail.com>
2021-11-05 09:44:41 +0000

    添加对命令行参数的解析

--- test/12command.c
+++ test/12command.c
@@ -1,8 +1,25 @@
 #include "00testhelper.h"
 
-int main(int argc, char *argv[])
+char **parseArgument(int argc, char *argv)
 {
-	// checkEquals(argc, 1, __LINE__);
-	// checkStringEquals(argv[0], "./z.out", __LINE__);
-	// return 0;
+	char** arr = (char**)lscmalloc(4* argc);
+	char *p = argv;
+	for (int i = 0; i < argc; i++)
+	{
+		arr[i] = p;
+		while (*p != '\0')
+		{
+			p++;
+		}
+		p++;
+	}
+	return arr;
+}
+
+int main(int argc, char *argv)
+{
+	char **args = parseArgument(argc, argv);
+	checkEquals(argc, 1, __LINE__);
+	checkStringEquals(args[0], "./z.out", __LINE__);
+	return 0;
 }
