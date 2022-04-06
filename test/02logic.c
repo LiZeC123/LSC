fcdc45f581b32d7a79da6a32ce27e6fb61be25a2
lizec <tomgreem123@gmail.com>
2022-04-06 14:52:45 +0800

    补充测试用例

--- test/02logic.c
+++ test/02logic.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+    // 基础逻辑判断
     int true = 1;
     int false = 0;
 
@@ -17,6 +18,7 @@ int main()
     checkTrue(!false, __LINE__);
     checkFalse(!true, __LINE__);
 
+    // 变量与常数混合判断
     int a = 1;
     int b = -1;
     checkTrue(1 == 1, __LINE__);
@@ -33,7 +35,17 @@ int main()
     checkTrue(-1 < 0, __LINE__);
     checkTrue(-1 <= 0, __LINE__);
 
-    checkTrue(1 == 1 && 2 == 2 && 2 > 1, __LINE__);
+    // 组合逻辑表达式
+    checkTrue(true && true && true, __LINE__);
+    checkFalse(true && false && true, __LINE__);
+    checkTrue(true || false || true, __LINE__);
+    checkFalse(false || false || false, __LINE__);
+    checkTrue(false || (true && true), __LINE__);
+    checkTrue(true || (false && true), __LINE__);
+
+    // 逻辑表达式与运算符号混合计算
+    checkTrue(true && 2 == 2 && 2 > 1, __LINE__);
+    checkTrue(false || 1 != 2 || 2 > 1, __LINE__);
 
     return 0;
 }
